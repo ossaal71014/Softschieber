31d7f19d8ff12c0e53ff72749a210d6089564499
ossaal71014 <73289000+ossaal71014@users.noreply.github.com>
2020-12-02 10:35:51 +0100

    Script Name und Klassen Name angeglichen

--- Spiel/Assets/Scripts/killaftertime.cs
+++ Spiel/Assets/Scripts/killaftertime.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class killaftertime : MonoBehaviour
 {
     public float zeit = 1f;
     // Start is called before the first frame update
