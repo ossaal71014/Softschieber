68865b70a4908730b48c8aaab2d321f9082b1b31
ossaal71014 <73289000+ossaal71014@users.noreply.github.com>
2020-12-02 11:14:53 +0100

    Update killdeath.cs

--- Spiel/Assets/Scripts/killdeath.cs
+++ Spiel/Assets/Scripts/killdeath.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class killdeath : MonoBehaviour
 {
     public GameObject explo;
+    public int damage = 1;                              //Variable für Schaden, der dem Schiff zugefügt werden soll
     // Start is called before the first frame update
     void Start()
     {
