aee711672b83eb1427fa20a76da7a5552a7f2421
ossaal71014 <73289000+ossaal71014@users.noreply.github.com>
2020-12-04 14:12:04 +0100

    Update projektil.cs

--- Spiel/Assets/Scripts/projektil.cs
+++ Spiel/Assets/Scripts/projektil.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class projektil : MonoBehaviour
 {
     public float speed = 2f;                            //Variable für Projektilgeschwindigkeit
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,7 @@ public class projektil : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Projektil soll sich mit Geschwindigkeit "speed" aufwärts bewegen
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
     }
 }
