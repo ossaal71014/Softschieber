50b5b8da4e93ce8aab90d5cd4faed698bea95f45
IlonaAravidou <73291621+IlonaAravidou@users.noreply.github.com>
2020-12-11 11:42:19 +0100

    Schussanzahl beschränkt
    Schussanzahl für einen kleinen Zeitraum beschränkt da es dann nicht so einfach macht die Meteoride abzuschießen

--- Spiel/Assets/Scripts/abschuss.cs
+++ Spiel/Assets/Scripts/abschuss.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class abschuss : MonoBehaviour
 {
     public GameObject typ1;                             //schuss1 (Projektil)
+    public float schussrate = 0.1f;
+    public float nachladezeit = 0.5f;
+    public int schussladung = 2;
+    public int schussladungMax = 2;
+    public bool jetzt = true;
+    private bool gestartet = false;
+    private bool amLaden = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +23,36 @@ public class abschuss : MonoBehaviour
     void Update()
     {
         //Abfrage: Bei Tastendruck der zugeordneten Aktion "Fire1" (Maustaste Default) soll Projektil abgefeuert werden
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && jetzt && schussladung > 0)
+        
         {
             //Schuss wird abgefeuert
             Instantiate(typ1, transform.position, Quaternion.identity);
+            jetzt = false;
+            schussladung--;
         }
+        if(!jetzt && !gestartet)
+        {
+            StartCoroutine(Wartezeit());
+            gestartet = true; 
+        }
+        if (schussladung < schussladungMax && !amLaden)
+        {
+            StartCoroutine(Nachladen());
+            amLaden = true;
+        }
+    }
+    IEnumerator Wartezeit()
+    {
+        yield return (new WaitForSeconds(schussrate));
+        jetzt = true;
+        gestartet = false; 
+    }
+    IEnumerator Nachladen() 
+    {
+        yield return (new WaitForSeconds(nachladezeit));
+        schussladung++;
+        amLaden = false; 
+
     }
 }
---
+++ Spiel/Assets/Scripts/schild.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class schild : MonoBehaviour
+{
+    public bool schildAn; //default false
+    private Animator anim;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (schildAn)
+        {
+            anim.SetBool("schildAn", true);
+            schildAn = false;
+            StartCoroutine(MachAus());
+
+        }
+    }
+    IEnumerator MachAus()
+    {
+        yield return (new WaitForSeconds(0.1f));
+        anim.SetBool("schildAn", false);
+    }
+}
