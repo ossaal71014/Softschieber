50776414cb2af404729529de7b5b3c8bb2033a5a
IlonaAravidou <73291621+IlonaAravidou@users.noreply.github.com>
2020-12-13 14:03:46 +0100

    schild-script erstellt
    schild-script für die Ausführung der Animation erstellt

---
+++ Spiel/Assets/Scripts/schild.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class schild : MonoBehaviour
+{
+    public bool schildAn;   //default-Wert: false
+    private Animator anim; //Beschreibung der boolschen Variable
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Schildanimation erstellen mit der Übergabe der schildAn-Variable von einem anderen script
+        if (schildAn)
+        {
+            anim.SetBool("schildAn", true);
+            schildAn = false; 
+            StartCoroutine(MachAus()); //SchildAn im Animator auf false setzen, damit es nicht wiederholt wird
+        }
+    }
+    IEnumerator MachAus()
+    {
+        yield return (new WaitForSeconds(0.1f));
+        anim.SetBool("schildAn", false);
+    }
+}
