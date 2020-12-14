1700df2f79062054375d3d69264addab6b396884
IlonaAravidou <73291621+IlonaAravidou@users.noreply.github.com>
2020-12-14 14:47:09 +0100

    GameOver Graphik & Punktezählen für Treffer und Kills

--- Spiel/Assets/Scripts/GUIScript.cs
+++ Spiel/Assets/Scripts/GUIScript.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class GUIScript : MonoBehaviour
 {
     //Graphiken für das Banner 
-    public GUISkin skin1;       
+    public GUISkin skin1;
+    public GUISkin skin2;
     public Texture2D shield;
     public Texture2D ship;
     public Texture2D background;
@@ -17,15 +18,24 @@ public class GUIScript : MonoBehaviour
 
     private string praefix = "";    //Darstellung Score mit Nullen davorgestellt z.B. 00018
 
+    private GameLogic gLogic;       //Verweis zum script GameLogic
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        gLogic = gameObject.GetComponent<GameLogic>();  
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (gLogic.spielStart)
+        {
+            score = 0;
+            ships = 3;
+            gLogic.spielStart = false;
+
+        }
         homi = Screen.width / 2;
         praefix = "00000";
 
@@ -63,6 +73,11 @@ public class GUIScript : MonoBehaviour
         GUI.Label(new Rect(homi - 60, 13, 50, 50), "" + shields);
         GUI.Label(new Rect(homi, 13, 200, 50), "score: " + praefix + score);
 
+        GUI.skin = skin2;
+        if (gLogic.gameOver)
+        {
+            GUI.Label(new Rect(homi - 150, Screen.height / 2 - 50, 300, 100), "game over");
+        }
 
     }
 }
