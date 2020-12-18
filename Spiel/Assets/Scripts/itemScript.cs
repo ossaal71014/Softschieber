b28d7da66fb525574b65df1351f3506d7285bbe9
IlonaAravidou <73291621+IlonaAravidou@users.noreply.github.com>
2020-12-18 10:36:32 +0100

    Töne bei Itemaufnahme

--- Spiel/Assets/Scripts/itemScript.cs
+++ Spiel/Assets/Scripts/itemScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class itemScript : MonoBehaviour
 {
-    public enum itemTyp { shieldAdd, shipAdd, weapon1};
+    public enum itemTyp {shieldAdd, shipAdd, weapon1}; 
     public itemTyp typ = itemTyp.shieldAdd;
     // Start is called before the first frame update
     void Start()
@@ -16,5 +16,37 @@ public class itemScript : MonoBehaviour
     void Update()
     {
         
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("killzone"))
+        {
+            Destroy(gameObject);
+        }
+
+        if (other.CompareTag("Player"))
+        {
+            GetComponent<Renderer>().enabled = false;
+            GetComponent<Collider2D>().enabled = false;
+            //renderer.enabled = false;
+            //collider2D.enabled = false;
+            // Zerstöre Obejkt erst nach 2f
+            Destroy(gameObject, 2f);
+            GetComponent<AudioSource>().Play();
+
+            //Schiff wird benachrichtigt das Schild, Schiff oder Waffe erhalten wurde
+            if (typ == itemTyp.shieldAdd)
+            {
+                other.SendMessage("ShieldAdd", SendMessageOptions.DontRequireReceiver);
+            }
+            if (typ == itemTyp.shipAdd)
+            {
+                other.SendMessage("ShipAdd", SendMessageOptions.DontRequireReceiver);
+            }
+            if (typ == itemTyp.weapon1)
+            {
+                other.SendMessage("Weapon1", SendMessageOptions.DontRequireReceiver);
+            }
+        }
     }
 }
