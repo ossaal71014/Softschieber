65cad710d75c7463ec744ed9de5909929ceec7b7
ossaal71014 <73289000+ossaal71014@users.noreply.github.com>
2020-12-22 15:17:30 +0100

    Update enemySchuss.cs

--- Spiel/Assets/Scripts/enemySchuss.cs
+++ Spiel/Assets/Scripts/enemySchuss.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class enemySchuss : MonoBehaviour
 {
-    public float speed = 0.5f;
+    public float speed = 2f;
     // Start is called before the first frame update
     void Start()
     {
